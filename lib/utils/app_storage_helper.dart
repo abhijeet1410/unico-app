342798025fd9b5ed57b699a408fc8d8a86b08618
KumarSunil17 <skmuduli17@gmail.com>
2022-07-12 12:28:42 +0530

    pre

--- lib/utils/app_storage_helper.dart
+++ lib/utils/app_storage_helper.dart
@@ -1,5 +1,4 @@
 import 'package:get_storage/get_storage.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 ///
 /// Created by Sunil Kumar
@@ -8,25 +7,15 @@ import 'package:shared_preferences/shared_preferences.dart';
 mixin AppStorageHelper {
   static const COUNTER_KEY = "counter";
   static final _appStorage = GetStorage();
-  static late SharedPreferences _preferences;
 
   static void init() async {
     // GetStorage.init();
     _appStorage.initStorage;
-    _preferences = await SharedPreferences.getInstance();
   }
 
   static Future<void> storeCounter(int counter) async {
     final now = DateTime.now();
-    print("STORE COUNTER");
-    print(now.difference(now).inMilliseconds);
-    _appStorage.write(COUNTER_KEY, counter).then((value) {
-      print(DateTime.now().difference(now).inMilliseconds);
-    });
-
-    _preferences.setInt(COUNTER_KEY, counter).then((value) {
-      print(DateTime.now().difference(now).inMilliseconds);
-    });
+    _appStorage.write(COUNTER_KEY, counter);
   }
 
   static int get counter => _appStorage.read<int>(COUNTER_KEY) ?? 0;
